2b5da35aee762c5664dcc282e5194c67736b9888
Poonammahunta <mahunta.poonam@gmail.com>
2018-10-12 07:39:57 +0530

    advance file added recently

---
+++ advance.py
@@ -0,0 +1,15 @@
+import os
+import sys
+var = raw_input("Enter the directory to search for : ")
+current_dir = os.path.realpath(os.path.dirname(__file__))
+#print current_dir
+#os.path.join(current_dir,var)
+list_duplicate = list()
+
+if not os.path.isdir(os.path.join(current_dir,var)):
+    print "Not Exist!!!"
+else:
+    for root,dirs,files in os.walk(os.path.join(current_dir,var)):
+        s = [files if files.endswith('.pdf')]
+        list_duplicate.append(s)        
+    print list_duplicate
